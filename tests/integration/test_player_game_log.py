0df8a9cac75d9200e358cb0bdbf0bbf169784df6
mjk2244 <mjk2244@columbia.edu>
2023-03-02 18:09:23 -0500

    reformatting

--- tests/integration/test_player_game_log.py
+++ tests/integration/test_player_game_log.py
@@ -1,5 +1,6 @@
 from src import player_game_log as p
 
+
 class TestClass:
     # INTEGRATION TESTS
     def test_get_player_game_log(self):
@@ -8,7 +9,6 @@ class TestClass:
 
         # Austin Ekeler had 13 rushing touchdowns in 2022
         assert p.get_player_game_log("Austin Ekeler", 2022)['Rush_TD'].sum() == 13
-        
+
         # Stefon Diggs had 1429 receiving yards in 2022
         assert p.get_player_game_log("Stefon Diggs", 2022)['Rec_Yds'].sum() == 1429
-        
