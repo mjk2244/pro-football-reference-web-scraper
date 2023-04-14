a9290f63c76367887b93569da61a6fec289d8767
mjk2244 <mjk2244@columbia.edu>
2023-04-14 17:00:38 -0400

    initial docs work for splits release

--- pro_football_reference_web_scraper/team_splits.py
+++ pro_football_reference_web_scraper/team_splits.py
@@ -138,4 +138,7 @@ def splits_sum(game_log: pd.DataFrame, grouping: str) -> pd.DataFrame:
 
     return game_log.iloc[::-1]
 
-    return game_log.iloc[::-1]
+def main():
+    home_road(team = 'Philadelphia Eagles', season = 2022, avg = True).to_markdown('test.txt')
+
+if __name__ == '__main__': main()
